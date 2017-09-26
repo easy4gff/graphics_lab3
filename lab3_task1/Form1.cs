f8a251a535e4ab88984181217d519f849f494147
easy4gff <giraffe_1996@mail.ru>
2017-09-26 23:03:44 +0300

    Попытка реализовать заполнение области цветом (пока что неудачная)

--- lab3_task1/Form1.cs
+++ lab3_task1/Form1.cs
@@ -13,9 +13,12 @@ namespace lab3_task1
     public partial class Form1 : Form
     {
         private Graphics g;
-        private bool leftButtonDown;
-        private Pen pen;
-        private Point prevCords;
+        private bool     leftButtonDown;
+        private Pen      pen;
+        private Pen      fillPen;
+        private Point    prevCords;
+        private Bitmap   originalImage;
+        private Color    chosenColor;
 
         private void drawPoint(int x, int y)
         {
@@ -36,15 +39,66 @@ namespace lab3_task1
             pictureBox1.Invalidate();
         }
 
+        private Point getLeftBound(Point p)
+        {
+            int y = p.Y;
+            int x = p.X;
+            while (x > 0 && chosenColor == originalImage.GetPixel(x - 1, y))
+                --x;
+            return new Point(x, y);
+        }
+
+        private Point getRightBound(Point p)
+        {
+            int y = p.Y;
+            int x = p.X;
+            int width = originalImage.Width;
+            while (x < width - 1 && chosenColor == originalImage.GetPixel(x + 1, y))
+                ++x;
+            return new Point(x, y);
+        }
+
+        private void floodFillByPicture(Point p)
+        {
+            if (originalImage.GetPixel(p.X, p.Y) != chosenColor) return;
+
+            Point leftBound = getLeftBound(p);
+            Point rightBound = getRightBound(p);
+            g.DrawLine(fillPen, leftBound, rightBound);
+            pictureBox1.Invalidate();
+
+            int leftX = leftBound.X;
+            int rightX = rightBound.X;
+
+            if (p.Y < originalImage.Height - 1)
+            {
+                for (int i = leftX; i <= rightX; ++i)
+                {
+                    floodFillByPicture(new Point(i, p.Y + 1));
+                }
+            }
+
+            if (p.Y > 0)
+            {
+                for (int i = leftX; i <= rightX; ++i)
+                {
+                    floodFillByPicture(new Point(i, p.Y - 1));
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            originalImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = originalImage;
             g = Graphics.FromImage(pictureBox1.Image);
             g.Clear(Color.White);
 
             leftButtonDown = false;
             pen = new Pen(Color.SlateBlue);
+            fillPen = new Pen(Color.Pink);
             //pen.Dispose();
         }
 
@@ -63,14 +117,25 @@ namespace lab3_task1
 
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
-            leftButtonDown = true;
-            prevCords = e.Location;
-            drawPoint(prevCords);
+            if (e.Button == MouseButtons.Left)
+            {
+                leftButtonDown = true;
+                prevCords = e.Location;
+                drawPoint(prevCords);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                chosenColor = originalImage.GetPixel(e.X, e.Y);
+                floodFillByPicture(e.Location);
+            }
         }
 
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
-            leftButtonDown = false;
+            if (e.Button == MouseButtons.Left)
+            {
+                leftButtonDown = false;
+            }
         }
     }
 }
