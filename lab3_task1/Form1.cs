e4846b39c41acdd0bb82f563a7fc510ca2a0bffb
easy4gff <giraffe_1996@mail.ru>
2017-09-26 21:49:04 +0300

    Добавлена возможность рисования области произвольной формы

--- lab3_task1/Form1.cs
+++ lab3_task1/Form1.cs
@@ -12,9 +12,65 @@ namespace lab3_task1
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private bool leftButtonDown;
+        private Pen pen;
+        private Point prevCords;
+
+        private void drawPoint(int x, int y)
+        {
+            g.DrawEllipse(pen, x, y, 1, 1);
+            pictureBox1.Invalidate();
+        }
+
+        private void drawPoint(Point p)
+        {
+            g.DrawEllipse(pen, p.X, p.Y, 1, 1);
+            pictureBox1.Invalidate();
+        }
+
+        private void drawLineTo(Point p)
+        {
+            g.DrawLine(pen, prevCords, p);
+            prevCords = p;
+            pictureBox1.Invalidate();
+        }
+
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+
+            leftButtonDown = false;
+            pen = new Pen(Color.SlateBlue);
+            //pen.Dispose();
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("image1.png");
+        }*/
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (leftButtonDown)
+            {
+                drawLineTo(e.Location);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            leftButtonDown = true;
+            prevCords = e.Location;
+            drawPoint(prevCords);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            leftButtonDown = false;
         }
     }
 }
